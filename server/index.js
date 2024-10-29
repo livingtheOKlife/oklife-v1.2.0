fed35aca7ae6938b9bafd35c0ddf9ec0bdb012d4
Oliver Kelly <oliver.philip.kelly@outlook.com>
2024-10-29 17:48:09 +0000

    backend - database  + backend/config/database.config.js  ^ server/index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser'
 
 import { notFound, errorHandler } from './middleware/error.middleware.js'
 
+import connectDB from './config/database.config.js'
+
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -18,4 +20,6 @@ app.get('/', () => console.log('Server is ready'))
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => {
+  connectDB(), console.log(`Server started on port ${port}`)
+})
