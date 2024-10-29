3e16ab3e665375d664658bdf376b573ed160a420
Oliver Kelly <oliver.philip.kelly@outlook.com>
2024-10-29 17:46:11 +0000

    backend - error handler  + backend/middleware/error.middleware.js  ^ server/index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import cookieParser from 'cookie-parser'
+
+import { notFound, errorHandler } from './middleware/error.middleware.js'
 
 const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 app.get('/', () => console.log('Server is ready'))
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
